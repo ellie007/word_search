d37db6598db74f1c6b945e7e66ce68913e090b7f
Eleanor Lee <ellie007@gmail.com>
2014-03-04 11:32:13 -0600

    now with vertical

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,40 +1,85 @@
 class Solver
 
-  def solve(grid_of_letters, words)
-    sorted_words = words.sort_by! { |word| word.length }.reverse!
-
+  def horiztonal(grid_of_letters, words)
     mutated_grid_of_letters = ""
     index = 0
 
     grid_of_letters.split("\n").each do |row|
-      sorted_words.each do |word|
+      words.each do |word|
         if row.include?(word)
           upcased_letters = row[row.index(word)..row.index(word)+word.length - 1].upcase!
           row[row.index(word)..row.index(word)+word.length - 1] = upcased_letters
         end
       end
+      mutated_grid_of_letters << grid_of_letters.split("\n")[index] = row + "\n"
+      index += 1
+    end
+    mutated_grid_of_letters
+  end
+
+
+  def vertical(grid_of_letters, words)
+    mutated_grid_of_letters = ""
+    index = 0
+
+    transpose(grid_of_letters).each do |row|
+      words.each do |word|
+        if row.include?(word)
+          upcased_letters = row[row.index(word)..row.index(word)+word.length - 1].upcase!
+          row[row.index(word)..row.index(word)+word.length - 1] = upcased_letters
+        end
+      end
+      mutated_grid_of_letters << grid_of_letters.split("\n")[index] = row + "\n"
+      index += 1
+    end
+    transpose(mutated_grid_of_letters).join("\n")
+  end
+
+
+  def transpose(grid_of_letters)
+    set_index = 0
+    sub_index = 0
+    greater_array = []
+
+    while sub_index < grid_of_letters.split("\n").length do
+      row = ""
+      while set_index < grid_of_letters.split("\n").length do
+        row << grid_of_letters.split("\n")[set_index][sub_index]
+        greater_array << row
+        set_index += 1
+      end
+      row = ""
+      set_index = 0
+      sub_index += 1
+    end
+    greater_array.uniq!
+  end
+
+
+  def solve(grid_of_letters, words)
+    mutated_grid_of_letters = ""
+    index = 0
+
+    horizontal_eval = horiztonal(grid_of_letters, words)
+
+    vertical(horizontal_eval, words).split("\n").each do |row|
       row.split("").each { |letter| row[letter] = "." if letter != letter.upcase }
       mutated_grid_of_letters << grid_of_letters.split("\n")[index] = row + ("\n")
       index += 1
     end
-    mutated_grid_of_letters.downcase!
+    mutated_grid_of_letters.downcase
   end
 
-  def vertical(grid_of_letters)
-    grid = []
-    grid_of_letters.split("\n").each do |row|
-      row.split("")
-      grid.push(row)
-    end
-    return grid
-    #return split_grid
-  end
 
 end
 
-puts Solver.new.solve("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["abc", "fgh", "klm", "pqr", "uvw"])
+print Solver.new.solve("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["abc", "fgh", "mrw", "nsx", "oty"])
 
-#puts Solver.new.vertical("abcde\nfghij\nklmno\npqrst\nuvwxy\n")
+#print Solver.new.transpose_grid("abcde\nfghij\nklmno\npqrst\nuvwxy\n")
+
+#print Solver.new.vertical("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["mrw", "nsx", "oty"])
+
+
 
 # def remove_unused_letters(row, word)
 #  index = 0
@@ -49,4 +94,18 @@ puts Solver.new.solve("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["abc", "fgh", "klm
 #   row
 # end
 
+# THIS IS THE HASH TRIAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# def hash
+
+
+# word = "DOGGHTCAT"
+# word_hash = [6, 7, 8, 0, 1, 2]
+
+# word_hash.each do |value|
+#   word_hash.include?
+
+#   indices.each do |index|
+# end
+
+# puts hash
 
