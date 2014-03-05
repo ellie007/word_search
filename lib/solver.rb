1d3b8c3ef1426c6598fd277224c26a1390d0ca91
Eleanor Lee <ellie007@gmail.com>
2014-03-05 16:02:32 -0600

    final refactor for now before adding more features

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,27 +1,23 @@
 class Solver
 
   def initialize(grid_of_letters, words)
-    @solution = ''
-    @grid_of_letters = grid_of_letters
+    @current_state = grid_of_letters
     @words = words
   end
 
 
   def horizontal
-    index = 0
-    @solution = @grid_of_letters.split("\n").each do |row|
+    @current_state = @current_state.split("\n").each do |row|
       find_words_in_row(row)
-      index += 1
     end.join("\n")
   end
 
 
   def vertical
-    index = 0
-    @solution = transpose.split("\n").each do |row|
+    @current_state = transpose(@current_state).split("\n").each do |row|
       find_words_in_row(row)
-      index += 1
     end.join("\n")
+    @current_state = transpose(@current_state)
   end
 
 
@@ -37,59 +33,27 @@ class Solver
   end
 
 
-  def transpose
-    split_grid = @solution.split("\n").map { |row| row.split("") }
+  def transpose(grid)
+    split_grid = grid.split("\n").map { |row| row.split("") }
     split_grid.transpose.map(&:join).join("\n")
   end
 
 
   def solve
-    horizontal
     vertical
+    horizontal
 
-    @solution = @solution.split("\n").each do |row|
+    @current_state = @current_state.split("\n").each do |row|
       row.split("").each { |letter| row[letter] = "." if letter != letter.upcase }
-      #@grid_of_letters.split("\n")[index] = row + ("\n")
-    end
-    @solution.join("\n")
+    end.join("\n")
   end
-
 
 end
 
-print Solver.new("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["abc", "oty"]).solve
+#print Solver.new("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["abc", "oty"]).solve
 
 #puts Solver.new.transpose("abcde\nfghij\nklmno\npqrst\nuvwxy\n")
 
 #print Solver.new.vertical("abcde\nfghij\nklmno\npqrst\nuvwxy\n", ["mrw", "nsx", "oty"])
 
-
-
-# def remove_unused_letters(row, word)
-#  index = 0
-#   row.split('').each do |row_letter|
-#     if word.include?(row_letter) && row[index..index + word.length-1] == word
-#       index += word.length
-#     else
-#       row[index] = "."
-#       index += 1
-#     end
-#   end
-#   row
-# end
-
-# THIS IS THE HASH TRIAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# def hash
-
-
-# word = "DOGGHTCAT"
-# word_hash = [6, 7, 8, 0, 1, 2]
-
-# word_hash.each do |value|
-#   word_hash.include?
-
-#   indices.each do |index|
-# end
-
-# puts hash
 
--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,11 +1,27 @@
-require 'word_search'
+require "solver"
 
-describe Word Search do
+describe Solver do
 
-  let(:word) { WordSearch.new }
+  let(:grid) { Solver.new("abc\ndef", ["ab", "cf"]) }
 
-  it 'should find a word' do
-    word.character_set('mrw')
+  it "should evaluate the grid horizontally" do
+    grid.horizontal.should == "ABc\ndef"
+  end
+
+  it "should evaluate the grid vertically" do
+    grid.vertical.should == "abC\ndeF"
+  end
+
+  it "transpose a vertical" do
+    grid.find_words_in_row("abc").should == "ABc\n"
+  end
+
+  it "should transpose the grid" do
+    grid.transpose("abc\ndef").should == "ad\nbe\ncf"
+  end
+
+  it "should use the solve method" do
+    grid.solve.should == "ABC\n..F"
   end
 
 end
